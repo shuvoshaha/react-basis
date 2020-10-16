1951a7df004f9c76bfae4f444930e8bd3683259e
shuvoshaha <shaha460@gmail.com>
2020-10-16 22:33:06 +0600

    create ES6 Arrow Function to Add and Delte Item

--- src/GoogleNote/Home.jsx
+++ src/GoogleNote/Home.jsx
@@ -3,26 +3,42 @@ import GoogleNoteMake from './GNote';
 import ViewNote from './NoteView';
 
 const Home = () => {
-    const [ data, setData ] = React.useState([])
-    function GetDataFromGNote(obj){
-        setData ((oldData) => {
-            return [ ...oldData, obj ]
+    const [data, setData] = React.useState([]);
+    
+    const GetDataFromGNote = (obj) => {
+        setData((oldData) => {
+            return [...oldData, obj]
         })
     }
-   // console.log(data);
+    const deleteItem = (id) =>{
+        setData ((oldData) => 
+        oldData.filter((val, index) =>{
+                return index !== id;
+
+        })
+        )
+    }
+    
     return (
         <div className="google-note-section">
-            <GoogleNoteMake  funcPass={GetDataFromGNote} />
-            {data.map(( value, index)  => {
-                return (
-                <ViewNote key={index} 
-                title={ value.title } 
-                message={ value.message }
-                />
-                );
-            })
-            
-            }
+            <GoogleNoteMake funcPass={GetDataFromGNote} />
+            <div className="g-note-section">
+                <div className="container">
+                    <div className="g-note-view">
+                        {data.map((value, index) => {
+                            return (
+                                <ViewNote key={index}
+                                    id={ index }
+                                    title={value.title}
+                                    message={value.message}
+                                    deleteItem = { deleteItem }
+                                />
+                            );
+                        })
+                        }
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
