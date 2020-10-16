7a3f91e91e4dd0b492140afeac9d7f7e963cb329
shuvoshaha <shaha460@gmail.com>
2020-10-16 21:03:12 +0600

    old data must return on seta data

--- src/GoogleNote/GNote.jsx
+++ src/GoogleNote/GNote.jsx
@@ -2,34 +2,46 @@ import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
 
-const GNote = () => {
+const GNote = (props) => {
     const [ value, setValue ] = useState({
         title: '',
-        message: ''
+        message: '',
     });
     //Get data from input field
-    onChangeHandler((e) => {
+   const onChangeHandler = (e) =>{
+       e.preventDefault();
+       const { name, value } = e.target;
+       setValue ((data) => {
+           return {
+               ...data,
+               [name] : value,
+           }
+       })
+   }
+    //Submit data
+    const submitHandler = ((e) => {
         e.preventDefault();
-        const { name, value } = e.target;
-        setValue({
-            [name] : value,
-        })
+        props.funcPass(value);
+        //console.log(value);
     })
 
     
     return (
+       
         <div className="google-note">
-            <form onSubmit={ submitHandler }>
+            <form onSubmit={submitHandler} >
                 <input
-                    name="note-field"
+                    name="title"
                     type="text"
                     className="note-title"
                     onChange={onChangeHandler} />
 
                 <textarea
-                    name=""
+                    name="message"
                     cols="30"
-                    rows="10">
+                    rows="10"
+                    onChange={ onChangeHandler }
+                    >
                 </textarea>
 
                 <Button
