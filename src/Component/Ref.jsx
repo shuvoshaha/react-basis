1590f9066bd674f6a4e97d01afefb0341777ce6e
shuvoshaha <shaha460@gmail.com>
2020-10-16 11:23:26 +0600

    Forward Ref Initialize

--- src/Component/Ref.jsx
+++ src/Component/Ref.jsx
@@ -1,16 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import RefForward from './RefForward'
 
 export default class Ref extends Component {
     constructor(){
         super();
         this.state = {
-            name: ''
+            name: '',
+            ref: ''
         }
          this.inputRef = React.createRef();
+         this.refForward = React.createRef();
          
     }
     componentDidMount(){
-        console.log(this.inputRef)
+        console.log(this.inputRef);
+        console.log(this.state.ref);
     }
     onChangeHandler(){
 
@@ -19,8 +23,11 @@ export default class Ref extends Component {
         return (
             <>
             <p>{this.state.name} </p>
+            <p> {this.state.ref} </p>
                <input className="text" type="text" ref={this.inputRef} onChange= {this.onChangeHandler} />
-               <button onClick={ () => this.setState({ name: this.inputRef.current.value }) } >Submit</button> 
+               <button onClick={ () => this.setState({ name: this.inputRef.current.value }) } >Submit</button> { <br />}
+               <RefForward ref={this.refForward} />
+               <button onClick={() => this.setState({ ref: this.refForward.current.value })} >Forward Ref</button>
             </>
         )
     }
